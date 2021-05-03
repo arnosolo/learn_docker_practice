88762616a97e5a5826ae3f6669a1d3ef671283d0
Arno Solo <46125820+arnosolo@users.noreply.github.com>
2021-05-04 06:58:45 +0800

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { MONGO_IP, MONGO_PASSWORD, MONGO_PORT, MONGO_USER } = require('./config/config');
+const postRouter = require("./routes/postRoutes")
+
+const app = express();
+
+app.use(express.json());
+
+// ****************************************************
+// Connect MongoDB
+const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+mongoose
+.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("succesfully connected to DB"))
+.catch(err => console.log(err));
+
+
+// ****************************************************
+// Handle Request
+app.get("/", (req, res) => {
+  res.send("<h2>Hello docker, I am Arno. I have </h2>");
+});
+
+// 
+app.use("/api/v1/posts", postRouter);
+
+
+// ****************************************************
+// Start Server
+const port = process.env.PORT || 3000;
+app.listen(port, () => { console.log(`listening on port ${port}`); })
